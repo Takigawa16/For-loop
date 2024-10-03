8e62044d86658b18414412efb62fe8d103d71273
Takigawa16 <regil99ariola@gmail.com>
2024-10-03 09:14:08 +0800

    Update problem2.py

---
+++ problem2.py
@@ -0,0 +1,9 @@
+list_of_fruits = ["banana", "apple", "orange", "mango"]
+
+for fruit in list_of_fruits:
+    if fruit == "banana":
+        print("hellow banana")
+    elif fruit == "apple":
+        print("hellow apple")
+    else:
+        print("no")
