8e97e25132a37470698debc1a2f83596588fda47
Takigawa16 <regil99ariola@gmail.com>
2024-10-03 09:13:44 +0800

    Update problem1.py

---
+++ problem1.py
@@ -0,0 +1,8 @@
+num = int(input(" Enter Number of fruits "))
+my_fav = []
+
+for i in range (num):
+    name = input (" Enter name of fruits ")
+    my_fav.append(name)
+  
+print(f"My favorite foods are {my_fav}")
